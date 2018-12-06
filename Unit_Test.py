f1f739db0454c9a420de519d1cb2b4f037c0e394
Sihyun Lee <lsh9034@kookmin.ac.kr>
2018-12-06 14:24:31 +0900

    Unit_Test

--- Unit_Test.py
+++ Unit_Test.py
@@ -14,14 +14,15 @@ class TestGuess(unittest.TestCase):
     def testDisplayCurrent(self):
         f = open("Test_Code.txt", "r")
         Data = f.read()
-        Data = Data.replace('\n', '')
+        Data = Data.replace('\n', ' ')
         Samples = Data.split("#")
         for i in range(len(Samples)):
             Samples[i] = Samples[i].split('_')
+            Samples[i][1] = Samples[i][1].rstrip()
             print(Samples[i])
 
-        self.assertEqual(self.interpreter.execute(Samples[0][0]), Samples[0][1])  # 답은 3
-        #self.assertEqual(self.interpreter.execute(Samples[1][0]), Samples[1][1])  # 답은 3
+        for i in range(len(Samples)):
+            self.assertEqual(self.interpreter.execute(Samples[i][0]), Samples[i][1])  # 답은 3
 
     def testDisplayGuessed(self):
         f = open("Test_Code.txt", "r")
