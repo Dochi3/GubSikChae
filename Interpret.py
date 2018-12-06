4c08e1c662e97d3b489f813a28ae36ef43942e52
sunjbs <sunjbs98@gmail.com>
2018-12-06 10:54:16 +0900

    Fix : Interpret.py

--- Interpret.py
+++ Interpret.py
@@ -10,27 +10,41 @@ class Interpreter:
 
     def interpret(self, codes):
         Words = codes.split()
-        opCodes = []
+        codes = []
         Stack = []
         check_Remark = True
         for idx, word in enumerate(Words):
             try:
-                opCode = wordToOpCode[word]
+                code = wordToOpCode[word]
             except:
                 raise Exception("Syntax Error")
 
-            if opCode == OpCodes.Op_Remark:
-                check_Remark = ~check_Remark
+            if code == OpCodes.Op_Remark:
+                check_Remark = not check_Remark
 
             if check_Remark:
-                if opCode == OpCodes.Op_Condition_Begin:
+                if code == OpCodes.Op_Condition_Begin:
                     Stack.append(idx)
-                if opCode == OpCodes.Op_Condition_End:
-                    opCodes[Stack[-1]] = (opCodes[Stack[-1]][0], idx)
+                if code == OpCodes.Op_Condition_End:
+                    codes[Stack[-1]] = (codes[Stack[-1]][0], idx)
                     idx = Stack[-1]
                     Stack.pop()
 
-                opCodes.append((opCode,idx))
-        return opCodes
+                codes.append((code, idx))
+            
+        return codes
 
-    def execute(self, codes):
+    def execute(self, codes, stdin):
+        codes = self.interpret(codes)
+        data = stdin.split()
+        isNumCode = lambda c: c == OpCodes.Op_Num_0 or c == OpCodes.Op_Num_1
+
+        for idx, code in enumerate(codes):
+            if isNumCode(code):
+                continue
+
+            if code == OpCodes.Op_List_Insert:
+                if idx + 1 < len(codes) and isNumCode(codes[idx + 1][1]):
+                    pass
+                else:
+                    raise Exception("Syntax Error")
