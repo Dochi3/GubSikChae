7e579c4a658badde6d41a91e0cef4e5446957e3e
Sihyun Lee <lsh9034@kookmin.ac.kr>
2018-12-06 14:08:23 +0900

    Unit_Test

--- Unit_Test.py
+++ Unit_Test.py
@@ -4,6 +4,7 @@ from Interpret import Interpreter
 
 class TestGuess(unittest.TestCase):
 
+
     def setUp(self):
         self.interpreter = Interpreter()
 
@@ -11,46 +12,28 @@ class TestGuess(unittest.TestCase):
         pass
 
     def testDisplayCurrent(self):
-        self.assertEqual(self.interpreter.interpret(), '_ e _ _ _ _ _ ')
-        self.g1.guess('a')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a _ _ _ ')
-        self.g1.guess('t')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a _ _ t ')
-        self.g1.guess('u')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess('z')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess('')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess(' ')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess('D')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess('d')
-        self.assertEqual(self.g1.displayCurrent(), 'd e _ a u _ t ')
-        self.g1.guess('f')
-        self.assertEqual(self.g1.displayCurrent(), 'd e f a u _ t ')
-        self.g1.guess('l')
-        self.assertEqual(self.g1.displayCurrent(), 'd e f a u l t ')
+        f = open("Test_Code.txt", "r")
+        Data = f.read()
+        Data = Data.replace('\n', '')
+        Samples = Data.split("#")
+        for i in range(len(Samples)):
+            Samples[i] = Samples[i].split('_')
+            print(Samples[i])
+
+        self.assertEqual(self.interpreter.execute(Samples[0][0]), Samples[0][1])  # 답은 3
+        #self.assertEqual(self.interpreter.execute(Samples[1][0]), Samples[1][1])  # 답은 3
 
     def testDisplayGuessed(self):
-        self.assertEqual(self.g1.displayGuessed(), ' e n ')
-        self.g1.guess('a')
-        self.assertEqual(self.g1.displayGuessed(), ' a e n ')
-        self.g1.guess('t')
-        self.assertEqual(self.g1.displayGuessed(), ' a e n t ')
-        self.g1.guess('u')
-        self.assertEqual(self.g1.displayGuessed(), ' a e n t u ')
-        self.g1.guess('d')
-        self.assertEqual(self.g1.displayGuessed(), ' a d e n t u ')
-        self.g1.guess('D')
-        self.assertEqual(self.g1.displayGuessed(), ' D a d e n t u ')
-        self.g1.guess('f')
-        self.assertEqual(self.g1.displayGuessed(), ' D a d e f n t u ')
-        self.g1.guess('l')
-        self.assertEqual(self.g1.displayGuessed(), ' D a d e f l n t u ')
-        self.g1.guess(' ')
-        self.assertEqual(self.g1.displayGuessed(),'   D a d e f l n t u ')
+        f = open("Test_Code.txt", "r")
+        Data = f.read()
+        Data = Data.replace('\n', '')
+        Samples = Data.split("#")
+        for i in range(len(Samples)):
+            Samples[i] = Samples[i].split('_')
+            print(Samples[i])
+        self.assertEqual(self.interpreter.execute(Samples[0][0]), Samples[0][1])  # 답은 3
+        self.assertEqual(self.interpreter.execute(Samples[1][0]), Samples[1][1])  # 답은 3
+        pass
 
 if __name__ == '__main__':
     unittest.main()
